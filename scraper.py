b196e58a0dad99067cd7a09ba70639e8b64479c6
Austin <30707585+Perfectdotexe@users.noreply.github.com>
2019-06-13 13:24:22 -0600

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import urllib.request as hyperlink
+import os
+
+link = hyperlink.urlopen('http://plugins.svn.wordpress.org/')
+wordPressSoup = BeautifulSoup(link,'lxml')
+filePath = os.path.dirname(os.path.realpath(__file__))
+fileNaming = (filePath + ('scrapedlist.txt'))
+for item in wordPressSoup.select("body", {"ul": "li"}):
+    print('The current working directory of the file is ' + filePath + ' the scraped list has been saved to this directory.')
+    with open(fileNaming, 'w') as output:
+        with open('fileNaming.txt'.format(wordPressSoup.text), mode='wt', encoding='utf-8') as file:
+            file.write(str(wordPressSoup.text.encode('utf-8')))
+    break
