a59755f67d77009b63f52d84534960d2455e740c
Austin <30707585+Perfectdotexe@users.noreply.github.com>
2019-06-15 01:03:30 -0600

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,9 +6,6 @@ link = hyperlink.urlopen('http://plugins.svn.wordpress.org/')
 wordPressSoup = BeautifulSoup(link,'lxml')
 filePath = os.path.dirname(os.path.realpath(__file__))
 fileNaming = (filePath + ('scrapedlist.txt'))
-for item in wordPressSoup.select("body", {"ul": "li"}):
-    print('The current working directory of the file is ' + filePath + ' the scraped list has been saved to this directory as scrapedlist.txt')
-    with open(fileNaming, 'wb') as output:
-        with open(b'scrapedlist.txt', mode='wb') as file:
-            file.write (bytes(wordPressSoup.text, 'utf8'))
-    break
+print('The current working directory of the file is ' + filePath + ' the scraped list has been saved to this directory as scrapedlist.txt')
+with open('scrapedlist.txt', 'wt', encoding='utf8') as file:
+        file.write(wordPressSoup.text)
