827069cd714df79bba2240676743491a5663254a
Austin <30707585+Perfectdotexe@users.noreply.github.com>
2019-06-14 22:08:53 -0600

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,7 +8,7 @@ filePath = os.path.dirname(os.path.realpath(__file__))
 fileNaming = (filePath + ('scrapedlist.txt'))
 for item in wordPressSoup.select("body", {"ul": "li"}):
     print('The current working directory of the file is ' + filePath + ' the scraped list has been saved to this directory as scrapedlist.txt')
-    with open(fileNaming, 'w') as output:
-        with open('scrapedlist.txt'.format(wordPressSoup.text), mode='wt', encoding='utf-8') as file:
-            file.write(str(wordPressSoup.text.encode('utf-8')))
-    sys.exit()
+    with open(fileNaming, 'wb') as output:
+        with open(b'scrapedlist.txt', mode='wb') as file:
+            file.write (bytes(wordPressSoup.text, 'utf8'))
+    break
